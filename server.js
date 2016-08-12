122ddd1fd1ed6e7448e0fea989c64711cbaa5d2c
Amit <amitrai9692@gmail.com>
2016-08-12 12:31:35 +0530

    small chane to check hook

--- server.js
+++ server.js
@@ -6,5 +6,8 @@ app.get('/',function(req,res){
     res.send("Hello Jenkins")
 });
 
+app. get('/hello',function(req,res){
+    var msg = "Hello " +  req.query.name || "Anonymous"
+});
 app.listen(process.env.PORT || 8084);
 module.exports = app;
